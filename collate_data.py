8fc786526e5884401530738ee4507aad746cd24e
Sarah Good <sarah.elizabeth.good@gmail.com>
2024-05-07 09:16:50 -0700

    Allow collation of data across other game

--- collate_data.py
+++ collate_data.py
@@ -7,6 +7,9 @@ from extract_startgg_data import startgg_vars, retryStrategy
 import requests
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
+from rapidfuzz import process, fuzz
+from concurrent.futures import ThreadPoolExecutor
+import math
 
 def safe_get(d, keys, default=None):
     """
@@ -173,7 +176,7 @@ def integrateStartGGPlayers(og_data_path='players.csv', reset_uid_ind = False):
     # Check for an existing data file and create or update accordingly
     if os.path.isfile('data\\players.csv'):
         df = pd.read_csv('data\\players.csv')
-        df['startgg_pid'] = df['startgg_pid'].astype(int)
+        df['startgg_pid'] = df['startgg_pid']
         if reset_uid_ind:
             df['uid'] = range(0, len(df))
     else:
@@ -202,7 +205,7 @@ def integrateStartGGPlayers(og_data_path='players.csv', reset_uid_ind = False):
     # Process each player in the CSV file
     for pid in startgg['player_id'].unique():
         # Only add if a new player id is encountered
-        if df.loc[df['startgg_pid'].astype(int) == pid,:].empty:
+        if df.loc[df['startgg_pid'] == pid,:].empty:
             data = fetchPlayerbyId(pid)
 
             if data:
@@ -268,9 +271,101 @@ def resolve_duplicates_strict(df, id_cols, newer_col):
     
     return id_df
 
+
+def filter_by_list_content(df, column_name, value):
+    # Filter rows where 'value' is in the list of 'column_name'
+    mask = df[column_name].apply(lambda x: value in str(x))
+    return df[mask]
+
+# Function to perform fuzzy matching and maintain associated data
+def fuzzy_match(row, master_list):
+    # Perform the fuzzy matching
+    master_list = list(master_list)
+    master_list_match = [x.lower() for x in master_list]
+    cleaned_names = [str(x).lower().replace(' ', '') for x in str(row['clean_name']).split('|')]
+    best_match = []
+    for clean_name in cleaned_names:
+        match = process.extractOne(clean_name, master_list_match, scorer=fuzz.WRatio, score_cutoff=97)
+        if match and best_match:
+            if match[1] > best_match[1]:
+                best_match = match
+        elif match:
+            best_match = match
+    # Return match details along with event_id and entrant_name
+    if best_match:
+        matched_index = best_match[2]  # Get the index of the matched entry
+        matched_data = master_list[matched_index]
+        
+        # Return match details along with event_id, entrant_name, and user_id from both sides
+        return pd.Series([row['uid'], best_match[0], best_match[1], row['player_name'],
+                          matched_data],
+                         index=['uid', 'matched_name', 'score', 'player_name',
+                                'liquidpedia_name'])
+    else:
+        # Return NaNs or some form of indication for no match found
+        return pd.Series([None, None, None, row['player_name'], None],
+                         index=['uid', 'matched_name', 'score', 'player_name',
+                                'liquidpedia_name'])
+    
+def batch_fuzzy_match(df, player_list, batch_size=10000, test=True):
+    results = []
+    for start in range(0, df.shape[0], batch_size):
+        end = min(start + batch_size, df.shape[0])
+        df_batch = df.iloc[start:end]
+        batch_results = df_batch.apply(fuzzy_match, axis=1, master_list=player_list)
+        results.append(batch_results)
+
+    return pd.concat(results).dropna(how='any')
+
+def merge_other_players(df, player_list, test=True):
+    unique_players = set(player_list)
+    cleaned_players = unique_players
+
+    match_me = df[['uid', 'player_name']]
+    match_me['clean_name'] = df['player_name'].str.lower().str.replace(' ', '', regex=True)
+
+    return batch_fuzzy_match(match_me, cleaned_players)
+
+def update_matched_values(df, matches, label, id_label):
+    for ind, row in matches.iterrows():
+        df.loc[df[id_label] == row[id_label], label] = row[label]
+    return df
+
+def insert_new(df, player_list, label, id_label):
+    player_list_unique = set(player_list)
+    df['match_row'] = df[label].str.lower().str.replace(' ', '', regex=True).str.split("|")
+    for player in player_list_unique:
+        player_lower = player.lower().replace(' ', '')
+        select = filter_by_list_content(df, 'match_row', player_lower)
+        if select.empty:
+            new_row = {
+                id_label: df.loc[:, id_label].max() + 1,
+                label: player
+            }
+
+            df = df._append(new_row, ignore_index=True)
+    
+    df = df.drop(columns=['match_row'])
+
+    return df
+
 if __name__ == '__main__':
-    #df = integrateStartGGPlayers(og_data_path='SF6\\all_sets.csv', reset_uid_ind=True)
-    df = pd.read_csv('data\\new_players.csv')
+    df = integrateStartGGPlayers(og_data_path='SFV\\all_sets.csv', reset_uid_ind=True)
+
+    sets1 = pd.read_csv('SF6\\all_sets.csv')
+    sets2 = pd.read_csv('SFV\\all_sets.csv')
+
+    events1 = pd.read_csv('SF6\\events.csv')
+    events2 = pd.read_csv('SFV\\events.csv')
+
+    events1['game'] = 'SF6'
+    events2['game'] = 'SFV'
+
+    sets1 = pd.concat([sets1, sets2], ignore_index=True)
+    sets1.to_csv('data\\all_sets.csv', index=False)
+
+    events = pd.concat([events1, events2], ignore_index=True).sort_values(['start_at', 'event_id'], ascending=[True, True])
+    events.to_csv('data\\events.csv', index=False)
 
     match_cols = ['discord_id', 'twitch_id', 'twitter_id']
 
